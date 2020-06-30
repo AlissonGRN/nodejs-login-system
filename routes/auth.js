b7ff52e5657d1a54c5789e3403a90b59d7f19ab0
AlissonGRN <guilhermeribeiro099@gmail.com>
2020-06-29 21:20:37 -0300

    created login route

--- routes/auth.js
+++ routes/auth.js
@@ -37,4 +37,23 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// Validate data and login user
+router.post("/login", async (req, res) => {
+  // validate user data before login
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // check if the email is valid
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).json({ error: "Email not found" });
+
+  // check if the password is correct
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).json({ error: "Invalid password" });
+
+  // create and assifn auth token
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+  res.header("auth-token", token).json({ token: token });
+});
+
 module.exports = router;
